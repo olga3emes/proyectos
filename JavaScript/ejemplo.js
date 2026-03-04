5396b78ae6b8480b42bfa855795c745356b83f9a
Olga M. <olga3emes@gmail.com>
2026-03-04 17:00:45 +0100

    Agregar archivos HTML y JavaScript iniciales con estructura básica y declaración 'use strict'

---
+++ JavaScript/ejemplo.js
@@ -0,0 +1,13 @@
+"use strict"
+const IVA = 22; //esta no se repite
+const ROJO = "#ff0000";
+const FECHANACIMIENTO="06/01/1994";
+const cumpleaños = "06/01/2027";
+
+let nombre = "Olga"; //no se repiten
+
+var apellido = "Moreno"; //esta sí se repite
+var apellido = "Martín";
+
+let productoPrecio = 19 * IVA;
+
