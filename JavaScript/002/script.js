6a39bdb73e93be25734c5d95048d3e32dcdba827
Olga M. <olga3emes@gmail.com>
2026-04-22 17:21:06 +0200

    clase 22.04

---
+++ JavaScript/002/script.js
@@ -0,0 +1,36 @@
+"use strict"
+
+let crear_nueva= "Hola!";
+
+const FECHA_NAC = "06/01/94";
+
+const birthday = "06/01/27";
+
+console.log(crear_nueva);
+
+
+console.info(`Esto es un aviso ${2+2}`);
+
+console.error("esto es un error");
+
+console.warn("esto warning");
+
+// let edad = Number(prompt("Introduce tu edad"));
+
+// console.info(edad);
+
+// let entra = confirm("esto entra en el examen?");
+
+// console.info(entra);
+
+let numeritos = "324";
+
+console.log (Number(numeritos));
+
+let a = 3;
+
+--a;
+
+a--;
+
+
